ad85d05bf35b71150f948e1f776e23bbdc424a44
Martin Isai <martinzptrms@gmail.com>
2023-02-20 08:55:16 -0600

    Init commit

---
+++ src/ClientJoy.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ToroDigital\Joy;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Collection;
+
+class ClientJoy
+{
+    private static $cookies;
+    private static $client;
+
+    public static function init($host)
+    {
+        self::$client = new Client([
+            'base_uri' => $host,
+            'headers' => [
+                'Accept' => 'application/json',
+            ]
+        ]);
+    }
+
+    public static function response($res)
+    {
+        return json_decode($res->getBody());
+    }
+
+    public static function jsonp_decode($jsonp, $assoc = false) {
+        $jsonp = (string)$jsonp;
+
+        if($jsonp[0] !== '[' && $jsonp[0] !== '{') { 
+            $jsonp = substr($jsonp, strpos($jsonp, '('));
+        }
+        $jsonp = trim($jsonp);      // remove trailing newlines
+        $jsonp = trim($jsonp,'()'); // remove leading and trailing parenthesis
+        
+        return json_decode($jsonp, $assoc);
+        
+    }
+
+    public static function getRanking($page = 1, $limit = 10)
+    {
+        $res = self::$client->get('/participations/v1/tickets-ranking-by-promo/62?page='.$page.'&limit='.$limit);
+
+        $ranking = self::jsonp_decode($res->getBody());
+
+        $ranking = new Collection($ranking->data);
+
+        $total = $ranking->count(); //all list count
+        $pages = ceil($total/$limit);
+
+        $ranking = $ranking->forPage($page,$limit);
+
+        return ['data' => $ranking, 'pages' => $pages, 'total' => $total ];
+    }
+    
+}
