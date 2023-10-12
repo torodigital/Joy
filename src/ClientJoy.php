f2b8ab2f6dc93ad336b68968279882c27da17cfc
Martín Isaí Zpt Rms <martinzptrms@gmail.com>
2023-10-12 03:12:51 -0600

    Update Joy v0.1.0

--- src/ClientJoy.php
+++ src/ClientJoy.php
@@ -2,23 +2,30 @@
 
 namespace ToroDigital\Joy;
 
+use Exception;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ServerException;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\ConnectException;
 use Illuminate\Support\Collection;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Exception\ConnectException;
+use stdClass;
 
 class ClientJoy
 {
+    private static $host = 'https://esiwxrbiye73aqmve16bn46cd.joyapp.mx';
+
     private static $cookies;
     private static $client;
-    private static $endpoint = '/participations/v1/tickets-ranking-by-promo/';
     private static $promotion_id;
 
-    public static function init($host, $promotion_id)
+    private static $endpoint = '/participations/v1/tickets-ranking-by-promo/';
+    private static $endPointBlock = '/promotions/v1/getBlocks';
+    private static $endPointBlockRanking = '/participations/v1/getRankingByBlocks';
+
+    public static function init($promotion_id)
     {
         self::$client = new Client([
-            'base_uri' => $host,
+            'base_uri' => self::$host,
             'headers' => [
                 'Accept' => 'application/json',
             ]
@@ -35,31 +42,54 @@ class ClientJoy
     public static function jsonp_decode($jsonp, $assoc = false) {
         $jsonp = (string)$jsonp;
 
-        if($jsonp[0] !== '[' && $jsonp[0] !== '{') { 
+        if($jsonp[0] !== '[' && $jsonp[0] !== '{') {
             $jsonp = substr($jsonp, strpos($jsonp, '('));
         }
         $jsonp = trim($jsonp);      // remove trailing newlines
         $jsonp = trim($jsonp,'()'); // remove leading and trailing parenthesis
-        
+
         return json_decode($jsonp, $assoc);
-        
     }
 
-    public static function getRanking($page = 1, $limit = 10)
+    public static function getBlocks($limit = 100000, $page = 1){
+        try{
+            $res = self::$client->get(self::$endPointBlock.'?promotion='.self::$promotion_id.'&page='.$page.'&limit='.$limit);
+
+            $blocks = self::jsonp_decode($res->getBody());
+
+            $data = new stdClass;
+            $data->data = $blocks->data->blocks;
+            $data->next_page = $blocks->data->next_page;
+            $data->prev_page = $blocks->data->prev_page;
+            $data->page = $blocks->data->pages;
+
+            return $data;
+
+        }catch(ServerException $se){
+            return ['data' => [], 'pages' => 0, 'next_page' => 0, 'prev_page' => 0];
+        }catch(ClientException $clientException){
+            return ['data' => [], 'pages' => 0, 'next_page' => 0, 'prev_page' => 0];
+        }catch(ConnectException $ce){
+            return ['data' => [], 'pages' => 0, 'next_page' => 0, 'prev_page' => 0];
+        }catch(Exception $e){
+            return ['data' => [], 'pages' => 0, 'next_page' => 0, 'prev_page' => 0];
+        }
+    }
+    
+    public static function getRanking($block_id, $page = 1, $limit = 10)
     {
         try{
-            $res = self::$client->get(self::$endpoint.self::$promotion_id.'?page='.$page.'&limit='.$limit);
-                    
+            $res = self::$client->get(self::$endPointBlockRanking.'?promotion='.self::$promotion_id.'&block_id='.$block_id.'&page='.$page.'&limit='.$limit);
             $ranking = self::jsonp_decode($res->getBody());
 
-            $ranking = new Collection($ranking->data);
+            $data = $ranking->ranking_blocks;
+            $data->links = self::pagination($data->links);
+            $ranking = new Collection($data->data);
 
-            $total = $ranking->count(); //all list count
-            $pages = ceil($total/$limit);
+            return ['data' => $ranking, 'pages' => $data->last_page,
+                    'current_page' => $data->current_page, 'links' => $data->links,
+                    'from' => $data->from, 'to' => $data->to, 'total' => 0];
 
-            $ranking = $ranking->forPage($page,$limit);
-
-            return ['data' => $ranking, 'pages' => $pages, 'current_page' => $page, 'total' => $total];
         }catch(ServerException $se){
             return ['data' => [], 'pages' => 0, 'current_page' => 0, 'total' => 0];
         }catch(ClientException $clientException){
@@ -70,5 +100,37 @@ class ClientJoy
             return ['data' => [], 'pages' => 0, 'current_page' => 0, 'total' => 0];
         }
     }
-    
+
+    public static function pagination($links){
+        $links = Collection::make($links);
+
+        $points = $links->map(function($item, $key){
+            return $item->label == "...";
+        })->reject(function($i){
+            return $i == false;
+        });
+
+        $keys = $points->keys();
+
+        $b = $links->countBy(function($l){
+            return $l->url == null;
+        });
+
+        if($points->count() == 2){
+            $links->splice(0, $keys[0] + 1);
+            $keys[1] = $keys[1] - ($keys[0] + 1);
+
+            $links->splice($keys[1], $links->count());
+        }
+        else if($points->count() == 1){
+            $links->splice($keys[0], $links->count());
+            $links->splice(0, 1);
+        }else if($b->count() == 2){
+            $links->splice(0, 1);
+            $links->splice(-1, 1);
+        }
+
+        return $links;
+    }
+
 }
